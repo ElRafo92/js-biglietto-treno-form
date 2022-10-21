0f501fbdf2b19ba28a9fdecdf65cc296cc4e118f
ElRafo92 <rafael.martinez@outlook.it>
2022-10-21 21:07:19 +0200

    creata prima parte grafica

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,32 @@
 // richiesta età e kilometri da percorrere
-const age = Number (prompt('Please insert your age:'));
-console.log (age)
+// const age = Number (prompt('Please insert your age:'));
+// console.log (age)
 
-const Km = Number (prompt('Please insert the Km of your travel:'));
-console.log (Km)
+// const Km = Number (prompt('Please insert the Km of your travel:'));
+// console.log (Km)
 
-const price = (Km * 0.21);
-console.log (price)
+// const price = (Km * 0.21);
+// console.log (price)
 
-let realPrice;
-let fixedNum;
-if (age < 18) {
-    realPrice = (price - (price * 0.2));
-    fixedNum = realPrice.toFixed(2);
-    console.log (fixedNum);
-    document.getElementById ('realPrice').innerHTML = `${fixedNum} $`;
-} else if (age >= 65){
-    realPrice = (price - (price * 0.4));
-    fixedNum = realPrice.toFixed(2);
-    console.log (fixedNum);
-    document.getElementById ('realPrice').innerHTML = `${fixedNum} $`;
-} 
-else {
-    const fixedPrice = price.toFixed(2);
-    console.log (fixedPrice);
-    document.getElementById ('realPrice').innerHTML = `${fixedPrice} $`; 
-}
+// let realPrice;
+// let fixedNum;
+// if (age < 18) {
+//     realPrice = (price - (price * 0.2));
+//     fixedNum = realPrice.toFixed(2);
+//     console.log (fixedNum);
+//     document.getElementById ('realPrice').innerHTML = `${fixedNum} $`;
+// } else if (age >= 65){
+//     realPrice = (price - (price * 0.4));
+//     fixedNum = realPrice.toFixed(2);
+//     console.log (fixedNum);
+//     document.getElementById ('realPrice').innerHTML = `${fixedNum} $`;
+// } 
+// else {
+//     const fixedPrice = price.toFixed(2);
+//     console.log (fixedPrice);
+//     document.getElementById ('realPrice').innerHTML = `${fixedPrice} $`; 
+// }
+
+const element = document.getElementById (`name`);
+console.log (element);
+document.getElementById ('gg').innerHTML = (element)
